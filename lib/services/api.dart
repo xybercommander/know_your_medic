d21414a755668f86daf6f4a4e53a8617cf275e07
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-04-03 14:24:08 +0530

    fixing the api

---
+++ lib/services/api.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class MedicApi {
+
+  Future<void> callSymptomsData() async {
+    try {
+      
+      var url = Uri.https('sandbox-healthservice.priaid.ch', '/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNhbXJhdG11a2hlcmplZS5zcHNAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI4ODQ3IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIxLTAzLTMxIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE2MTc0NDY0NzQsIm5iZiI6MTYxNzQzOTI3NH0.YPy5Fr4CTpjDQVzwHrgVf5PiDY8AiI_RknC4OqP4QVg&format=json&language=en-gb');
+      Response response = await get(url);
+      Map data = jsonDecode(response.body);
+      print(data);
+
+    } catch(e) {
+      print(e.toString());
+    }
+  }
+
+}
--- lib/views/UserPages/user_symptoms_page.dart
+++ lib/views/UserPages/user_symptoms_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:know_your_medic/services/api.dart';
 
 class UserSymptomsPage extends StatefulWidget {
   @override
@@ -6,12 +7,18 @@ class UserSymptomsPage extends StatefulWidget {
 }
 
 class _UserSymptomsPageState extends State<UserSymptomsPage> {
+  MedicApi medicApi = MedicApi();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(      
       body: Center(
-        child: Text('Symptoms'),
+        child: IconButton(
+          icon: Icon(Icons.info),
+          onPressed: () {
+            medicApi.callSymptomsData();
+          },
+        ),
       )
     );
   }
