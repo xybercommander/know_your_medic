6594f55ea265ad56ddec21ed53132f68f69bfadf
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-04-04 10:28:39 +0530

    fixing diagnosis list

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -39,7 +39,7 @@ class MedicApi {
       var url = Uri.parse('https://healthservice.priaid.ch/diagnosis?symptoms=$symptoms_ID&gender=$apiGender&year_of_birth=$age&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNhbXJhdG11a2hlcmplZS5zcHNAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2MjA2IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMTA5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6IjEwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IkJhc2ljIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMS0wMy0zMSIsImlzcyI6Imh0dHBzOi8vYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTYxNzUxNTYyNiwibmJmIjoxNjE3NTA4NDI2fQ.wDFewlImdDkkpZ3F4X2m8HiylrBS3H57AZl_wZg-o2Q&format=json&language=en-gb');
       Response response = await get(url);
       var data = jsonDecode(response.body);
-      print(data);
+      print(data.length);
 
       // print(Symptoms.symptoms[index]['ID']);
       
---
+++ lib/views/UserPages/diagnosis.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiagnosisPage extends StatefulWidget {
+  @override
+  _DiagnosisPageState createState() => _DiagnosisPageState();
+}
+
+class _DiagnosisPageState extends State<DiagnosisPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO: Add the swiper package and make the swiper with a new diagnosis module containing a list
+    );
+  }
+}
