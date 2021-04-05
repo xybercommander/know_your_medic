adccad3086a459b0378e3727349e07c378cad249
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-04-05 10:11:24 +0530

    removed the issues page

--- lib/views/user_navigator.dart
+++ lib/views/user_navigator.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:know_your_medic/views/UserPages/user_issues_page.dart';
-
 import 'package:know_your_medic/views/UserPages/user_symptoms_page.dart';
 import 'package:know_your_medic/views/UserPages/user_profile_page.dart';
 
@@ -11,7 +9,7 @@ class UserNavigator extends StatefulWidget {
 
 class _UserNavigatorState extends State<UserNavigator> {
   PageController pageController = PageController(initialPage: 0);
-  List<Widget> pages = [UserSymptomsPage(), UserIssuesPage(), UserProfilePage()];
+  List<Widget> pages = [UserSymptomsPage(), UserProfilePage()];
   int _selectedIndex = 0;
 
 
@@ -21,12 +19,7 @@ class _UserNavigatorState extends State<UserNavigator> {
             fontFamily: 'Quicksand-Bold',
             fontSize: 24
           ),);
-    if (index == 1) return Text('Issues', style: TextStyle(
-            color: Theme.of(context).primaryColor,
-            fontFamily: 'Quicksand-Bold',
-            fontSize: 24
-          ),);
-    if (index == 2) return Text('Account', style: TextStyle(
+    if (index == 1) return Text('Account', style: TextStyle(
             color: Theme.of(context).primaryColor,
             fontFamily: 'Quicksand-Bold',
             fontSize: 24
@@ -75,15 +68,8 @@ class _UserNavigatorState extends State<UserNavigator> {
             title: Container(),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.info,
-                color: _selectedIndex == 1
-                    ? Theme.of(context).primaryColor
-                    : Colors.grey[400]),
-            title: Container(),
-          ),
-          BottomNavigationBarItem(
             icon: Icon(Icons.account_box_rounded,
-                color: _selectedIndex == 2
+                color: _selectedIndex == 1
                     ? Theme.of(context).primaryColor
                     : Colors.grey[400]),
             title: Container(),
