920c4d0ff90fbababd003fd8fdca17122b8209c3
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-04-08 10:47:32 +0530

    added the database functions

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:know_your_medic/modules/staff_constants.dart';
+import 'package:know_your_medic/modules/user_constants.dart';
 
 class DatabaseMethods {
 
@@ -22,5 +24,62 @@ class DatabaseMethods {
       .snapshots();
   }
 
+  // adding a message to chats sub-collection of chatrooms collection
+  Future addMessage(String chatRoomId, String messageId, Map messageInfoMap) {
+    return FirebaseFirestore.instance.collection('chatrooms')
+      .doc(chatRoomId)
+      .collection('chats')
+      .doc(messageId)
+      .set(messageInfoMap);
+  }
 
+  // Updating the last message sent collections
+  updateLastMessageSent(String chatRoomId, Map lastMessageInfoMap) {
+    return FirebaseFirestore.instance.collection('chatrooms')
+      .doc(chatRoomId)
+      .update(lastMessageInfoMap);
+  }
+
+  // Creating or searching for new or existing chatrooms
+  createChatRoom(String chatRoomId, Map chatRoomInfoMap) async {
+    final snapshot = await FirebaseFirestore.instance.collection('chatrooms')
+      .doc(chatRoomId)
+      .get();
+
+    if(snapshot.exists) {
+      // if chatroom exists
+      return true;
+    } else {
+      // if chatroom does not exist
+      return FirebaseFirestore.instance.collection('chatrooms')
+        .doc(chatRoomId)
+        .set(chatRoomInfoMap);
+    }
+  }
+
+  // retrieving the chat messages
+  Future<Stream<QuerySnapshot>> getChatRoomMessages(chatRoomId) async {
+    return FirebaseFirestore.instance.collection('chatrooms')
+      .doc(chatRoomId)
+      .collection('chats')
+      .orderBy('ts', descending: true)
+      .snapshots();
+  }
+
+  // Getting the list of all the chatrooms created
+  Future<Stream<QuerySnapshot>> getChatRooms(isStaff) async {
+    String name;
+    if(isStaff) {
+      name = StaffConstants.name;
+    } else {
+      name = UserConstants.name;
+    }
+
+    return FirebaseFirestore.instance.collection('chatrooms')
+      .orderBy('lastMessageSentTs', descending: true)
+      .where('users', arrayContains: name)
+      .snapshots();
+  }
+
+  
 }
